8e16d37c27f4d4cf6dfa22b58af35b416ca4137a
pknot212121 <pknot@student.agh.edu.pl>
2025-11-03 14:25:16 +0100

    Start

--- README.md
+++ README.md
@@ -1,3 +1,63 @@
-# FluidDynamics
-Projekt na Symulacje Systemów Dyskretnych
-aaa
+# gl-pic-fluid
+
+*Realtime 3D particle-in-cell fluid simulation*
+
+Based on Robert Bridson's *Fluid Simulation for Computer Graphics, 2nd ed.*
+
+Implements NVidia's ["Screen Space Fluid Rendering for Games"](http://developer.download.nvidia.com/presentations/2010/gdc/Direct3D_Effects.pdf).
+
+[Video Demo](https://www.youtube.com/watch?v=lNG8B_eWKaw)
+
+<img src="promo3.png" alt="promo picture" style="margin: auto;"/>
+
+## Requirements
+
+* OpenGL >= 4.3
+* cmake >= 3.10
+* C++ compiler for version >= 17
+
+## Building
+
+1. `git submodule update --init --recursive`
+2. `mkdir build`
+3. `cd build`
+4. `cmake ..`
+5. Use generated build system
+
+## Running
+
+1. `cd build`
+2. `bin/fluid`
+
+Controls:
+* Left click and drag to interact with fluid
+* Right click and drag to rotate the view
+* `space` - play/pause
+* `s` - step
+* `r` - reset
+* `f` - toggle screen space fluid rendering
+* `p` - toggle particle visibility (for viewing grid)
+* PIC/FLIP blending controls
+    * `home` - set FLIP 0.9
+    * `end` - set FLIP 0.0 (PIC)
+    * `page up` - increase FLIP
+    * `page down` - decrease FLIP
+* Grid visualization controls
+    * `g` - toggle grid visualization
+    * `1` - cell type
+    * `2` - velocity
+    * `3` - divergence
+    * `4` - pressure solver A coefficients
+    * `5` - pressure
+
+## Known Bugs
+
+* There is a lingering boundary condition bug which causes fluid to stick to positive direction walls.
+    * The stickiness is patched in the pressure update shader, but a bias in the fluid pressures remains.
+* If fluid gathers in the +XYZ corner of the cube, it can cause the simulation to explode. I expect that this is related to or is a result of the previous bug.
+    * Press `r` to restart the simulation if this happens
+
+## Development
+
+* Simulation code is split across `src/Fluid.hpp` and `*.cs.glsl` shaders in `shader/`
+* Make sure to list new source files in CMakeLists.txt!
---
+++ src/gfx/object.hpp
@@ -0,0 +1,221 @@
+#pragma once
+#include <initializer_list>
+#include <memory>
+#include <sstream>
+#include <stdexcept>
+#include <unordered_map>
+#include <unordered_set>
+#include <vector>
+#include <glad/glad.h>
+
+namespace gfx {
+static constexpr glm::uint NOT_INSTANCED = 0;
+static constexpr glm::uint INSTANCED = 1;
+
+class Buffer {
+    int _size = 0, _length = 0;
+    void create() {
+        if (id)
+            return;
+        glGenBuffers(1, &id);
+    }
+
+    void destroy() {
+        if (!id)
+            return;
+        glDeleteBuffers(1, &id);
+    }
+public:
+    const glm::uint target;
+    glm::uint id = 0;
+    
+    Buffer(glm::uint target) : target(target) {}
+
+    ~Buffer() {
+        destroy();
+    }
+
+    void bind() const {
+        if (!id)
+            throw std::runtime_error("Buffer not initialized.");
+        glBindBuffer(target, id);
+    }
+    
+    void unbind() const {
+        if (!id)
+            throw std::runtime_error("Buffer not initialized.");
+        glBindBuffer(target, 0);
+    }
+
+    int size() const {
+        return _size;
+    }
+
+    int length() const {
+        return _length;
+    }
+    
+    Buffer& bind_base(glm::uint index) {
+        create();
+        glBindBufferBase(target, index, id);
+        return *this;
+    }
+
+    template <typename T>
+    void set_data(std::vector<T> data, GLenum usage = GL_STATIC_DRAW) {
+        create();
+        glBindBuffer(target, id);
+        glBufferData(target, sizeof(T) * data.size(), data.data(), usage);
+        glBindBuffer(target, 0); // unbind
+        _length = data.size();
+        _size = data.size() * sizeof(T);
+    } 
+
+private:
+    struct GlMappedBufferDeleter {
+        GLenum target;
+        glm::uint id;
+        void operator()(void* ptr) {
+            glBindBuffer(target, id);
+            glUnmapBuffer(target);
+            glBindBuffer(target, 0);
+        }
+    };
+
+public:
+    template <typename T>
+    std::unique_ptr<T[], GlMappedBufferDeleter> map_buffer() {
+        if (!id)
+            throw std::runtime_error("Buffer not initialized.");
+        bind();
+        std::unique_ptr<T[], GlMappedBufferDeleter> ptr(static_cast<T*>(glMapBuffer(target, GL_READ_WRITE)), GlMappedBufferDeleter{target, id});
+        unbind();
+        return ptr;
+    }
+
+    template <typename T>
+    const std::unique_ptr<T[], GlMappedBufferDeleter> map_buffer_readonly() const {
+        if (!id)
+            throw std::runtime_error("Buffer not initialized.");
+        bind();
+        std::unique_ptr<T[], GlMappedBufferDeleter> ptr(static_cast<T*>(glMapBuffer(target, GL_READ_ONLY)), GlMappedBufferDeleter{target, id});
+        unbind();
+        return ptr;
+    }
+};
+
+class VAO {
+    const std::unordered_map<GLenum, glm::uint> type_size{{
+        {GL_BYTE, 1},
+        {GL_UNSIGNED_BYTE, 1},
+        {GL_SHORT, 2},
+        {GL_UNSIGNED_SHORT, 2},
+        {GL_INT, 4},
+        {GL_UNSIGNED_INT, 4},
+        {GL_HALF_FLOAT, 2},
+        {GL_FLOAT, 4},
+        {GL_DOUBLE, 8},
+        {GL_FIXED, 4}
+    }};
+    const std::unordered_set<GLenum> integer_types{
+        GL_BYTE,
+        GL_UNSIGNED_BYTE,
+        GL_SHORT,
+        GL_UNSIGNED_SHORT,
+        GL_INT,
+        GL_UNSIGNED_INT
+    };
+    int auto_attrib_counter = 0;
+
+    void create() {
+        if (id)
+            return;
+        // create VAO, which encapsulates the vertex buffer and its layout
+        // https://www.khronos.org/opengl/wiki/Vertex_Specification#Vertex_Array_Object
+        glGenVertexArrays(1, &id);
+    }
+
+    void destroy() {
+        if (!id)
+            return;
+        glDeleteVertexArrays(1, &id);
+    }
+
+public:
+    glm::uint id = 0;
+
+    VAO() {}
+
+    ~VAO() {
+        destroy();
+    }
+
+    void bind() const {
+        if (!id)
+            throw std::runtime_error("VAO not initialized.");
+        glBindVertexArray(id);
+    }
+
+    void unbind() const {
+        if (!id)
+            throw std::runtime_error("VAO not initialized.");
+        glBindVertexArray(0);
+    }
+
+    VAO& attrib_index(glm::uint index) {
+        auto_attrib_counter = index;
+        return *this;
+    }
+
+    VAO& bind_attrib(const Buffer& buffer, glm::uint num_components, GLenum type, glm::uint divisor = NOT_INSTANCED) {
+        return bind_attrib(buffer, 0, type_size.at(type) * num_components, num_components, type, divisor);
+    }
+
+    VAO& bind_attrib(const Buffer& buffer, glm::uint offset_bytes, glm::uint stride, glm::uint num_components, GLenum type, glm::uint divisor) {
+        if (buffer.id == 0) {
+            throw std::runtime_error("Buffer not initialized.");
+        }
+
+        const glm::uint i = auto_attrib_counter;
+        if (i >= GL_MAX_VERTEX_ATTRIBS) {
+            std::stringstream str;
+            str << "Current attribute index " << i;
+            str << " is >= max of " << GL_MAX_VERTEX_ATTRIBS;
+            throw std::runtime_error(str.str());
+        }
+
+        create();
+        bind();
+
+        glEnableVertexAttribArray(i);
+
+        // binds the VBO to a VAO attribute
+        // https://www.khronos.org/opengl/wiki/Vertex_Specification#Separate_attribute_format
+        glBindVertexBuffer(
+            i, // binding (attribute) index
+            buffer.id, // buffer
+            offset_bytes, // offset of first element
+            stride // stride
+        );
+        
+        if (integer_types.count(type)) {
+            glVertexAttribIFormat(i, num_components, type, 0);
+        } else if (type == GL_DOUBLE) {
+            glVertexAttribLFormat(i, num_components, type, 0);
+        } else {
+            glVertexAttribFormat(
+                i, // attribute index
+                num_components, // size (number of components)
+                type, // type of components
+                GL_FALSE, // normalize integer type?
+                0 // "added to the buffer binding's offset to get the offset for this attribute"
+            );
+        }
+
+        glVertexBindingDivisor(i, divisor);
+        ++auto_attrib_counter;
+        unbind();
+        return *this;
+    }
+};
+}
